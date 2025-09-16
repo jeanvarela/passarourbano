b10527cc13bd9aa0e1cf0fa6ad44999c529c93fa
Jean Varela <jean.varela1@gmail.com>
2025-09-16 17:42:25 -0300

    Refatorada a ordem de compra para utilizar Reactive-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
 
@@ -39,7 +39,7 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [],
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from './ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -11,11 +11,14 @@ import { NgForm } from '@angular/forms';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra: number
 
-  @ViewChild('formulario')
-  public formulario: NgForm
-
-  public idPedidoCompra: number;
+  public formulario: FormGroup = new FormGroup({
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+    'complemento': new FormControl(null),
+    'formaPagamento': new FormControl(null, [Validators.required])
+  });
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
@@ -23,15 +26,19 @@ export class OrdemCompraComponent implements OnInit {
     
   }
 
-  public confirmarCompra(formulario: NgForm): void {
-    this.formulario = formulario
-    let pedido = new Pedido(this.formulario.value.endereco, this.formulario.value.numero,
-                            this.formulario.value.complemento, this.formulario.value.formaPagamento
-    );
-    this.ordemCompraService.efetivarCompra(pedido)
-                           .subscribe((response: any) => {
-                              this.idPedidoCompra = response.id;
-                           })
-    
+  public confirmarCompra(): void {
+    console.log('Confirma compra')
+    if (this.formulario.status === 'INVALID'){
+      this.formulario.get('endereco').markAsTouched()
+      this.formulario.get('numero').markAsTouched()
+      this.formulario.get('formaPagamento').markAsTouched()
+    }else{
+        let pedido: Pedido = new Pedido(this.formulario.value.endereco, this.formulario.value.numero,
+                                        this.formulario.value.complemento, this.formulario.value.formaPagamento)
+        this.ordemCompraService.efetivarCompra(pedido)
+                               .subscribe((response: any) => {
+                                   this.idPedidoCompra = response.id
+                               })                                
+    }
   }
 }
